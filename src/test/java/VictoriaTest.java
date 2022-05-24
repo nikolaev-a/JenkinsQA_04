5145a6097050e7aa9643fecd7d079ab1e12a8a4b
vn <victorianovakvn@gmail.com>
2022-05-23 21:09:34 -0400

    CourseraTest/Corrections

--- src/test/java/VictoriaTest.java
+++ src/test/java/VictoriaTest.java
@@ -8,17 +8,15 @@ public class VictoriaTest extends BaseTest {
 
     @Test
     public void CourseraTest() {
-        getDriver().get("https://www.coursera.org/");
-        WebElement searchBox = getDriver().findElement(By.xpath("//div[@class='react-autosuggest__container']//input"));
-        searchBox.sendKeys("Test automation");
-        WebElement search = getDriver().findElement(By.xpath("//header/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/form[1]/div[1]/div[1]/div[1]/button[2]/div[1]/*[1]"));
-        search.click();
-        search = getDriver().findElement(By.xpath("//header/div[2]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[2]/form[1]/div[1]/div[1]/div[1]/div[1]/input[1]"));
-        Assert.assertEquals(search.getAttribute("value"), "Test automation");
-        WebElement preferredCourse = getDriver().findElement(By.xpath("//h2[contains(text(),'Building Test Automation Framework using Selenium ')]"));
-        preferredCourse.click();
+        String SearchBoxXpath = "//div[@class='react-autosuggest__container']//input";
+        String search = "//button[2][@aria-label=\"Submit Search\"]//div[@class=\"magnifier-wrapper\"]";
 
+        getDriver().get("https://www.coursera.org/");
+        getDriver().findElement(By.xpath(SearchBoxXpath)).sendKeys("Test automation");;
+        getDriver().findElement(By.xpath(search)).click();
+        Assert.assertEquals(getDriver().findElement(By.xpath(SearchBoxXpath)).getAttribute("value"), "Test automation");
     }
 }
+
 
 
