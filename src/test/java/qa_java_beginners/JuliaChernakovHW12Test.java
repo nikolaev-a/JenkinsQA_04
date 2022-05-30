f3a6d8fdbccae30fd37144b97cecf20a27dbb42c
Julia Chernakov <julia.chernakov@gmail.com>
2022-05-30 10:21:44 +0300

    WebTestVerifyText

---
+++ src/test/java/qa_java_beginners/JuliaChernakovHW12Test.java
@@ -0,0 +1,42 @@
+package qa_java_beginners;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class JuliaChernakovHW12Test {
+    /**
+     * Подтвердите, что в меню BROWSE LANGUAGES, подменю  J, пользователь
+     * может найти описание страницы, на которой перечеслены все программные языки,
+     * начинающиеся с буквы J, отсортированные по названию
+     */
+    @Test
+    public void test() {
+        String chromeDriver = "webdriver.chrome.driver";
+        String driverPath = "/Applications/chromedriver";
+        String url = "http://www.99-bottles-of-beer.net/";
+        String expectedResult = "All languages starting with the letter J are shown, sorted by Language.";
+
+        System.setProperty(chromeDriver, driverPath);
+        WebDriver driver = new ChromeDriver();
+
+        driver.get(url);
+
+        WebElement menuBrowseLanguages = driver.findElement(By.xpath("//li/a[@href='/abc.html']"));
+        menuBrowseLanguages.click();
+
+        WebElement submenuJ = driver.findElement(By.xpath("//a[@href='j.html']"));
+        submenuJ.click();
+
+        WebElement text = driver.findElement(By.xpath("//p[strong]"));
+        String actualResult = "All languages starting with the letter J are shown, sorted by Language.";
+
+        Assert.assertEquals(actualResult, expectedResult);
+
+        driver.quit();
+    }
+}
+
