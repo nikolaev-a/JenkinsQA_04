33568de4d35b0e3862baf19cdeb5766d592e2d8f
StanislavL <stanislav_lsv@yahoo.com>
2022-06-05 14:22:43 -0400

    TC_001_000 | <Sing in>

---
+++ src/test/java/runner/StasSingInTest.java
@@ -0,0 +1,39 @@
+package runner;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class StasSingInTest extends BaseTest {
+
+    private WebDriver driver;
+
+    @BeforeMethod
+    @Override
+    protected void beforeMethod() {
+        driver = BaseUtils.createDriver();
+    }
+
+    @AfterMethod
+    @Override
+    protected void afterMethod() {
+        driver.quit();
+    }
+
+    @Test
+    public void testSignIn() {
+
+        driver.get("http://localhost:8080");
+
+        driver.findElement(By.name("j_username")).sendKeys("Some admin");
+        driver.findElement(By.name("j_password")).sendKeys("Some password");
+        driver.findElement(By.name("Submit")).click();
+
+        String actualResult = driver.findElement(By.cssSelector(".alert.alert-danger")).getText();
+        Assert.assertEquals(driver.getCurrentUrl(), "http://localhost:8080/loginError");
+        Assert.assertEquals(actualResult, "Invalid username or password");
+    }
+}
