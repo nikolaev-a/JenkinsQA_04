8f3de94cd600b5f1cf5686b5e58cc58f48fc2767
Dastan Torokul <torokul@yahoo.com>
2022-06-05 19:56:33 -0500

    Added JavascriptExecutor line 22

--- src/test/java/DastanAddDescriptionForProjectNameTest.java
+++ src/test/java/DastanAddDescriptionForProjectNameTest.java
@@ -1,5 +1,8 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
@@ -8,15 +11,16 @@ public class DastanAddDescriptionForProjectNameTest extends BaseTest {
 
     private final String PROJECT_NAME = "Some_Project";
 
-    protected void addDescription() throws InterruptedException {
+    protected void addDescription() {
         getDriver().findElement(By.cssSelector("span[class='task-link-text']")).click();
         WebElement entryField = getDriver().findElement(By.id("name"));
         entryField.sendKeys(PROJECT_NAME);
         getDriver().findElement(By.cssSelector("li[tabindex='0'][class='hudson_model_FreeStyleProject']")).click();
         getDriver().findElement(By.xpath("//button[@type='submit']")).click();
         getDriver().findElement(By.name("description")).sendKeys("This is a sample project.");
-        Thread.sleep(3000);
-        getDriver().findElement(By.xpath("//*[@id=\"yui-gen25-button\"]")).click();
+        WebElement submitButton = getDriver().findElement(By.xpath("//*[@id=\"yui-gen25-button\"]"));
+        JavascriptExecutor jse = (JavascriptExecutor) getDriver();
+        jse.executeScript("arguments[0].click()", submitButton);
     }
     protected void deleteProject() {
         getDriver().findElement(By.cssSelector("img#jenkins-head-icon")).click();
@@ -26,7 +30,7 @@ public class DastanAddDescriptionForProjectNameTest extends BaseTest {
     }
 
     @Test
-    public void descriptionTest() throws InterruptedException {
+    public void descriptionTest(){
         final String expectedText = "This is a sample project.";
         addDescription();
         String actualText = getDriver().findElement(By.xpath("//*[@id=\"description\"]/div[1]")).getText();
