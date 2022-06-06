1920441a754271cd5ce6f2c012f19ee1ece3ace2
Dastan Torokul <torokul@yahoo.com>
2022-06-06 01:03:02 -0500

    Changed xpath

--- src/test/java/DastanAddDescriptionForProjectNameTest.java
+++ src/test/java/DastanAddDescriptionForProjectNameTest.java
@@ -41,7 +41,7 @@ public class DastanAddDescriptionForProjectNameTest extends BaseTest {
         //FluentWait<WebDriver> fluentWait = new FluentWait<>(getDriver()).withTimeout(30, TimeUnit.SECONDS).pollingEvery(200, TimeUnit.MILLISECONDS).ignoring(NoSuchElementException.class);
         //WebDriverWait wait = new WebDriverWait(getDriver(), 10);
         //wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/div[4]/div[2]/div[1]/div[1]")));
-        String actualText = getDriver().findElement(By.xpath("/html/body/div[4]/div[2]/div[1]/div[1]")).getText();
+        String actualText = getDriver().findElement(By.xpath("//*[contains(text(), 'This is a sample project.')]")).getText();
         Assert.assertEquals(actualText, expectedText);
         deleteProject();
     }
