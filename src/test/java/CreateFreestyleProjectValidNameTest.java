9e73faaf2accb20c96e207891a1a840874ef3e83
umaybeen <umaybeen@gmail.com>
2022-06-07 16:10:53 -0700

    CreateFreestyleProjectValidNameTest

---
+++ src/test/java/CreateFreestyleProjectValidNameTest.java
@@ -0,0 +1,55 @@
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import runner.BaseTest;
+
+
+public class CreateFreestyleProjectValidNameTest extends BaseTest {
+
+    @Test
+    public void testCreateFreestyleProjectValidName ()  {
+
+        String expectedResult = "MarinaTest";
+
+        getDriver().findElement(By.xpath("//div[@id='tasks']//a[@href='/view/all/newJob']")).click();
+        getDriver().findElement(By.id("name")).sendKeys("MarinaTest");
+        getDriver().findElement(
+                By.xpath("//div[@id='j-add-item-type-standalone-projects']//" +
+                        "ul[@class='j-item-options']//li[@class='hudson_model_FreeStyleProject']")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+        getDriver().findElement(By.id("yui-gen25-button")).click();
+        getDriver().findElement(By.xpath("//ul[@id='breadcrumbs']//a[@href='/']")).click();
+
+        String actualResult = getDriver().findElement(
+                By.id("main-panel")).findElement(By.linkText("MarinaTest")).getText();
+
+        Assert.assertEquals(actualResult,expectedResult);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
+
+
+
+
+
