44f6a33eccd03e3efd87dc2ace8b11720b53a5e3
dmswlpk <103320248+dmswlpk@users.noreply.github.com>
2022-06-07 23:07:51 -0600

    TC_001.015/AT_001.015

--- src/test/java/CreateFreestyleFolderAddDescription.java
+++ src/test/java/CreateFreestyleFolderAddDescription.java
@@ -5,34 +5,46 @@ import org.testng.annotations.Test;
 
 import runner.BaseTest;
 
-public class CreateFreestyleFolderAddDescription extends BaseTest {
+import java.util.PrimitiveIterator;
 
-    private void deleteNewProject(String link) {
+public class CreateFreestyleFolderAddDescription extends BaseTest {
+    private final String NEW_ITEM = "New Item";
+    private final String INPUT_NAME = "name";
+    private final String OK_BUTTON = "ok-button";
+    private final String SAVE_BUTTON = "//button[@type='submit']";
+    private final String DELETE_PROJECT_BUTTON = "Delete Project";
+    private final String EDIT_DESCRIPTION = "//a[@href='editDescription']";
+    private final String EDIT_DESCRIPTION_TEXTAREA = "//textarea[@name='description']";
+    private final String SAVE_BUTTON_DESCRIPTION = "yui-gen1-button";
+
+
+
+    protected void deleteNewProject(String link) {
         getDriver().findElement(By.linkText(link)).click();
-        getDriver().findElement(By.linkText("Delete Project")).click();
+        getDriver().findElement(By.linkText(DELETE_PROJECT_BUTTON)).click();
 
         getDriver().switchTo().alert().accept();
     }
 
-    private void clearDescriptionBoxOfFreestyleProject() {
-        getDriver().findElement(By.id("description-link")).click();
+    protected void clearDescriptionBoxOfFreestyleProject() {
+        getDriver().findElement(By.xpath(EDIT_DESCRIPTION)).click();
         if (getDriver().findElement(By.name(("description"))).isDisplayed()) {
             getDriver().findElement(By.name("description")).clear();
-            getDriver().findElement(By.id("yui-gen1-button")).click();
+            getDriver().findElement(By.id(SAVE_BUTTON_DESCRIPTION)).click();
         }
     }
 
     @Test
     public void createFreestyleProject() {
-        getDriver().findElement(By.linkText("New Item")).click();
-        getDriver().findElement(By.id("name")).sendKeys("project 10");
+        getDriver().findElement(By.linkText(NEW_ITEM)).click();
+        getDriver().findElement(By.id(INPUT_NAME)).sendKeys("project 10");
         getDriver().findElement(By.xpath("//div[@id='j-add-item-type-standalone-projects']/ul//li[1]")).click();
-        getDriver().findElement(By.id("ok-button")).click();
+        getDriver().findElement(By.id(OK_BUTTON)).click();
 
         JavascriptExecutor js = (JavascriptExecutor) getDriver();
         js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
 
-        getDriver().findElement(By.xpath("//button[@type='submit']")).click();
+        getDriver().findElement(By.xpath(SAVE_BUTTON)).click();
 
         Assert.assertEquals(getDriver().getTitle(), "project 10 [Jenkins]");
 
@@ -40,11 +52,10 @@ public class CreateFreestyleFolderAddDescription extends BaseTest {
     }
 
     @Test
-    public void testAddDescriptionToFreestyleProject() {
-        getDriver().findElement(By.xpath("//a[@href='editDescription']")).click();
-        getDriver().findElement(By.xpath("//textarea[@name='description']")).sendKeys("new description added");
-
-        getDriver().findElement(By.id("yui-gen1-button")).click();
+    public void testAddDescriptionToFreestyleProject() throws InterruptedException {
+        getDriver().findElement(By.xpath(EDIT_DESCRIPTION)).click();
+        getDriver().findElement(By.xpath(EDIT_DESCRIPTION_TEXTAREA)).sendKeys("new description added");
+        getDriver().findElement(By.id(SAVE_BUTTON_DESCRIPTION)).click();
 
         Assert.assertEquals(getDriver().findElement(
                 By.xpath("//div[@id='description']/div[text()]")).getText(), "new description added");
