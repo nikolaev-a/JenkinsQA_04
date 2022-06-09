fce1de86f7792fe0ec81525fcb143e9e2fc748fd
Stanislaw1349 <alfstas@gmail.com>
2022-06-09 23:21:54 +0300

    corrected the mistake

--- src/test/java/DreamTeamJavaGroupTest.java
+++ src/test/java/DreamTeamJavaGroupTest.java
@@ -1,16 +1,14 @@
 import org.openqa.selenium.By;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
 public class DreamTeamJavaGroupTest extends BaseTest {
 
     public static final String FOOTER_REST_API = "//div[@class='page-footer__links rest_api hidden-xs']";
-    public static final String REST_API_PAGE_HEADER = "//div[@id='main-panel']/h1";
+    public static final String REST_API_PAGE_HEADER = "/h1";
 
-    @Ignore
-    @Test(enabled = false)
+    @Test
     public void testTC_132_001_FooterCheckLinksFelix_IX() {
 
         getDriver().findElement(By.xpath(FOOTER_REST_API)).click();
