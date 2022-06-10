99022b4355208a0c43da5e99b385c07a9e7cf3e3
Aliaksandr Dubouski <sashok2000888@gmail.com>
2022-06-10 23:40:54 +0300

    Add TC_001_037_CreateNewFreestyleProject_AD

--- src/test/java/DreamTeamJavaGroupTest.java
+++ src/test/java/DreamTeamJavaGroupTest.java
@@ -8,6 +8,7 @@ public class DreamTeamJavaGroupTest extends BaseTest {
 
     public static final String FOOTER_REST_API = "//div[@class='page-footer__links rest_api hidden-xs']";
     public static final String REST_API_PAGE_HEADER = "//div[@id='main-panel']/h1";
+    public static final String ITEMNAME = "Valid item name" + (int) (Math.random() * 1000);
 
     @Ignore
     @Test(enabled = false)
@@ -15,5 +16,18 @@ public class DreamTeamJavaGroupTest extends BaseTest {
 
         getDriver().findElement(By.xpath(FOOTER_REST_API)).click();
         Assert.assertEquals(getDriver().findElement(By.xpath(REST_API_PAGE_HEADER)).getText(), "REST API");
+    }
+
+    @Test
+    public void testTC_001_037_CreateNewFreestyleProject_AD() {
+        getDriver().findElement(By.className("task-link-text")).click();
+        getDriver().findElement(By.id("name")).sendKeys(ITEMNAME);
+        getDriver().findElement(By.className("hudson_model_FreeStyleProject")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+        getDriver().findElement(By.id("yui-gen19-button")).click();
+        getDriver().findElement(By.id("yui-gen25-button")).click();
+        getDriver().findElement(By.xpath("//a[text()='Dashboard']")).click();
+        String temp = getDriver().findElement(By.id("job_" + ITEMNAME)).getText();
+        Assert.assertTrue(temp.contains(ITEMNAME));
     }
 }
