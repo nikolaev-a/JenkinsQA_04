480bddfb92c0308b1e7a65b1e5f8398d79feb9a5
umaybeen <umaybeen@gmail.com>
2022-06-10 00:30:29 -0700

    fixing ci build issue

--- src/test/java/CreateFreestyleProjectValidNameMarinaSTest.java
+++ src/test/java/CreateFreestyleProjectValidNameMarinaSTest.java
@@ -8,30 +8,38 @@ import runner.BaseTest;
 
 public class CreateFreestyleProjectValidNameMarinaSTest extends BaseTest {
 
+    private final String FREESTYLE_PROJECT_NAME = "MarinaTest2";
+
+    private void deleteProject(String name) {
+        if (name != null && !name.equals("")) {
+            getDriver()
+                    .findElement(By.id("job_" + FREESTYLE_PROJECT_NAME))
+                    .findElement(By.className("jenkins-table__link"))
+                    .click();
+            getDriver().findElement(By.xpath("//a[@data-url='/job/" + FREESTYLE_PROJECT_NAME + "/doDelete']"))
+                    .click();
+            getDriver().switchTo().alert().accept();
+        }
+    }
+
     @Test
     public void testCreateFreestyleProjectValidName() {
 
-        String expectedResult = "MarinaTest2";
-
         getDriver().findElement(By.xpath("//div[@id='tasks']//a[@href='/view/all/newJob']")).click();
-        getDriver().findElement(By.id("name")).sendKeys("MarinaTest2");
+        getDriver().findElement(By.id("name")).sendKeys(FREESTYLE_PROJECT_NAME);
         getDriver()
                 .findElement(By.id("j-add-item-type-standalone-projects"))
                 .findElement(By.xpath("//li[@class='hudson_model_FreeStyleProject']/label"))
                 .click();
         getDriver().findElement(By.id("ok-button")).click();
-
-        WebElement saveButton = getWait5().until(
-                ExpectedConditions.visibilityOfElementLocated(By.id("yui-gen25-button"))
-        );
-        saveButton.click();
-
+        getDriver().findElement(By.id("yui-gen25-button")).click();
         getDriver().findElement(By.xpath("//ul[@id='breadcrumbs']//a[@href='/']")).click();
-
         String actualResult = getDriver().findElement(
                 By.id("main-panel")).findElement(By.linkText("MarinaTest2")).getText();
 
-        Assert.assertEquals(actualResult, expectedResult);
+        Assert.assertEquals(actualResult, FREESTYLE_PROJECT_NAME);
+
+        deleteProject(FREESTYLE_PROJECT_NAME);
     }
 }
 
