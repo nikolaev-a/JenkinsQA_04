8e5432f8daeb141707c3685b33adea8d84d5d93e
umaybeen <umaybeen@gmail.com>
2022-06-09 23:22:25 -0700

    fixing ci build issue

--- src/test/java/CreateFreestyleProjectValidNameMarinaSTest.java
+++ src/test/java/CreateFreestyleProjectValidNameMarinaSTest.java
@@ -15,15 +15,17 @@ public class CreateFreestyleProjectValidNameMarinaSTest extends BaseTest {
 
         getDriver().findElement(By.xpath("//div[@id='tasks']//a[@href='/view/all/newJob']")).click();
         getDriver().findElement(By.id("name")).sendKeys("MarinaTest2");
-        getDriver().findElement(
-                By.xpath("//div[@id='j-add-item-type-standalone-projects']//" +
-                        "ul[@class='j-item-options']//li[@class='hudson_model_FreeStyleProject']")).click();
+        getDriver()
+                .findElement(By.id("j-add-item-type-standalone-projects"))
+                .findElement(By.xpath("//li[@class='hudson_model_FreeStyleProject']/label"))
+                .click();
         getDriver().findElement(By.id("ok-button")).click();
 
         WebElement saveButton = getWait5().until(
-                ExpectedConditions.elementToBeClickable(By.id("yui-gen25-button"))
+                ExpectedConditions.visibilityOfElementLocated(By.id("yui-gen25-button"))
         );
         saveButton.click();
+
         getDriver().findElement(By.xpath("//ul[@id='breadcrumbs']//a[@href='/']")).click();
 
         String actualResult = getDriver().findElement(
