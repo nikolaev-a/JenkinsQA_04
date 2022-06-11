8db9e0c219b58e72f751c8600657c0461fe462bf
RusAli <mr.al1mov.ruslan@gmail.com>
2022-06-11 22:30:24 +0300

    clean code

--- src/test/java/MultiConfigurationProjectTest.java
+++ src/test/java/MultiConfigurationProjectTest.java
@@ -9,29 +9,12 @@ import runner.BaseTest;
 public class MultiConfigurationProjectTest extends BaseTest {
   private final String NAME_FOLDER = "TestMultiConfigurationProject";
 
-  protected void createMultiConfigFolder() {
-    getDriver().findElement(By.linkText("New Item")).click();
-    WebElement itemName = getDriver().findElement(By.id("name"));
-    itemName.sendKeys(NAME_FOLDER);
-    getDriver().findElement(By.xpath("//div[@id='j-add-item-type-standalone-projects']//li[3]")).click();
-    getDriver().findElement(By.xpath("//button[@id='ok-button']")).click();
-  }
-
   protected void createMultiConfigFolder(String name) {
     getDriver().findElement(By.linkText("New Item")).click();
     WebElement itemName = getDriver().findElement(By.id("name"));
     itemName.sendKeys(name);
     getDriver().findElement(By.xpath("//div[@id='j-add-item-type-standalone-projects']//li[3]")).click();
     getDriver().findElement(By.xpath("//button[@id='ok-button']")).click();
-  }
-
-  protected void deleteFolder(){
-
-    Actions action = new Actions(getDriver());
-    action.moveToElement(getDriver().findElement(
-            By.xpath("//a[@href='job/" + NAME_FOLDER + "/']"))).click().build().perform();
-    getDriver().findElement(By.xpath("//span[text()='Delete Multi-configuration project']")).click();
-    getDriver().switchTo().alert().accept();
   }
 
   protected void deleteFolder(String name){
@@ -45,15 +28,6 @@ public class MultiConfigurationProjectTest extends BaseTest {
 
   protected void returnToMainPage() {
     getDriver().findElement(By.id("jenkins-home-link")).click();
-  }
-
-  protected boolean isElementPresent() {
-    try {
-      getDriver().findElement(By.xpath("//tr[@id='job_" + NAME_FOLDER + "']//td[3]"));
-      return true;
-    } catch (org.openqa.selenium.NoSuchElementException e) {
-      return false;
-    }
   }
 
   protected boolean isElementPresent(String name) {
@@ -83,7 +57,7 @@ public class MultiConfigurationProjectTest extends BaseTest {
   @Test
   public void testCreateMultiConfigFolder_TC_041_001() {
 
-    createMultiConfigFolder();
+    createMultiConfigFolder(NAME_FOLDER);
     returnToMainPage();
 
     WebElement nameOnDashboard = getDriver().findElement(By.xpath("//tr[@id='job_" + NAME_FOLDER + "']//td[3]"));
